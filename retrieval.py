aee0e6d93e8d2b5f1a532ef3070f2067a74b5bf6
CMACH508 <52200550+CMACH508@users.noreply.github.com>
2022-10-24 17:37:01 +0800

    Add files via upload

--- retrieval.py
+++ retrieval.py
@@ -1,4 +1,4 @@
-#   Copyright 2021 Sicong Zang
+#   Copyright 2022 Sicong Zang
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -11,12 +11,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-#
-#   P.S. We thank Ha and Eck [1] for their public source codes.
-#        And the details about their work can be found below.
-#
-#       [1] https://github.com/tensorflow/magenta/tree/master/magenta/models/sketch_rnn
-#
+
 """ Calculate Ret."""
 
 import random
